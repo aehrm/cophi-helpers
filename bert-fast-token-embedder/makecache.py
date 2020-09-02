9584d5f657d4ddb651d4aa90b3a0f60bbeda843b
Anton Ehrmanntraut <anton@ehrmanntraut.de>
2020-09-02 15:43:00 +0200

    Reformat makecache.py

--- bert-fast-token-embedder/makecache.py
+++ bert-fast-token-embedder/makecache.py
@@ -16,7 +16,6 @@ cachewordfile = sys.argv[1]
 processed_dir = sys.argv[2]
 outputfile = sys.argv[3]
 
-
 print('Initializing model', file=sys.stderr)
 
 # adjust your model here
@@ -28,16 +27,12 @@ model.to('cuda')
 
 model.eval()
 torch.set_grad_enabled(False)
-max_piecelength = 8
 
 with open(cachewordfile) as f:
     cachewords = [x.strip() for x in f.readlines()]
 
 word_index_map = dict(zip(cachewords, range(len(cachewords))))
 tokenized_docs = [os.path.join(processed_dir, f) for f in os.listdir(processed_dir)]
-
-frequencies = torch.zeros([len(cachewords)], dtype=torch.int)
-accumulated_layers = torch.zeros([len(cachewords), max_piecelength, 13, 768], dtype=torch.float32)
 
 
 def read_doc(path):
@@ -84,7 +79,16 @@ def token_ranges(seq):
         yield (token, t[0][0], t[-1][0])
 
 
-def process_output(batch, output):
+num_sents = sum(map(len, map(read_doc, tokenized_docs)))
+max_piecelength = max(map(lambda x: len(tokenizer.tokenize(x)), cachewords))
+frequencies = torch.zeros([len(cachewords)], dtype=torch.int)
+accumulated_layers = torch.zeros([len(cachewords), max_piecelength, 13, 768], dtype=torch.float32)
+
+sents = tqdm(more_itertools.flatten(map(read_doc, tokenized_docs)), total=num_sents)
+batches = more_itertools.chunked(make_sequences(sents), 10)
+
+for batch in batches:
+    output = forward_pass(batch)
     for i, seq in enumerate(batch):
         for token, begin, end in token_ranges(seq):
             if token not in cachewords:
@@ -92,16 +96,9 @@ def process_output(batch, output):
 
             # shift by one due to [CLS]
             tensor = torch.cat(
-                [output[i].narrow(0, 1 + begin, end - begin + 1), torch.zeros(max_piecelength - end + begin - 1, 13, 768)], 0)
+                [output[i].narrow(0, 1 + begin, end - begin + 1),
+                 torch.zeros(max_piecelength - end + begin - 1, 13, 768)], 0)
             frequencies[word_index_map[token]] += 1
             accumulated_layers[word_index_map[token]] += tensor
-
-
-num_sents = sum(map(len, map(read_doc, tokenized_docs)))
-sents = tqdm(more_itertools.flatten(map(read_doc, tokenized_docs)), total=num_sents)
-batches = more_itertools.chunked(make_sequences(sents), 10)
-
-for batch in batches:
-    process_output(batch, forward_pass(batch))
 
 torch.save({'frequencies': frequencies, 'accumulated_layers': accumulated_layers, 'words': word_index_map}, outputfile)
