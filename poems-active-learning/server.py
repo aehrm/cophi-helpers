569dfd4e2647c9758f859b0ddf650aeb5691d404
Anton Ehrmanntraut <anton@ehrmanntraut.de>
2020-09-23 17:52:45 +0200

    Add error handling

--- poems-active-learning/server.py
+++ poems-active-learning/server.py
@@ -1,8 +1,23 @@
+import asyncio
 import sys
+import traceback
+
 from training_manager import TrainingManager
 from aiohttp import web
 import json
 import pandas
+
+
+async def start_train_wrapper(app):
+    def callback(f):
+        try:
+            f.result()
+        except:
+            traceback.print_exc()
+            print("Shutdown")
+            asyncio.get_running_loop().create_task(app.shutdown())
+
+    asyncio.wrap_future(app['manager'].start_training()).add_done_callback(callback)
 
 
 async def status(request):
@@ -26,7 +41,7 @@ async def start(request):
     if request.app['manager'].is_training():
         return web.json_response({'error': 'Model is training'}, status=500)
     else:
-        request.app['manager'].start_training()
+        await start_train_wrapper(request.app)
         return web.json_response({'message': 'Model started training'})
 
 
@@ -48,9 +63,11 @@ async def index(request):
     raise web.HTTPFound('/static/labelview.html')
 
 
+async def shutdown_handler(app):
+    sys.exit(1)
+
 app = web.Application()
 app['manager'] = TrainingManager(document_dir='./poems', labeled_documents_file='./labeled_documents.tsv')
-app['manager'].start_training()
 
 app.router.add_get('/api/status', status)
 app.router.add_get('/api/starttrain', start)
@@ -58,4 +75,7 @@ app.router.add_get('/api/labelbatch', label_batch)
 app.router.add_post('/api/uploadbatch', upload_batch)
 app.router.add_static('/static', 'static')
 app.router.add_get('/', index)
+app.on_startup.append(start_train_wrapper)
+app.on_shutdown.append(shutdown_handler)
+
 web.run_app(app, host='127.0.0.1', port=9999 if len(sys.argv) < 2 else int(sys.argv[1]))
